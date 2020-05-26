91429801a1111835b076bec700f8f1fe356e22df
Givbygo <bogdanzaliskiy@gmail.com>
2020-05-26 20:43:19 +0300

    include build for CD

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
@@ -67,6 +66,3 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `yarn build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
-=======
-# zaliskyipm
->>>>>>> 7839a74028871794b64d6fe439f5284365d072f8
---
+++ build/precache-manifest.ecae241dface2479365f35dbdf07a5da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef019ce2897f4883a9b42863c716141",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1caf02aea4fd31718b88",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1a28466fc3043aca0d6e",
+    "url": "/static/js/2.b7b5cafe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b7b5cafe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1caf02aea4fd31718b88",
+    "url": "/static/js/main.6a640266.chunk.js"
+  },
+  {
+    "revision": "bb33b7903493a8cd9930",
+    "url": "/static/js/runtime-main.d5395674.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
