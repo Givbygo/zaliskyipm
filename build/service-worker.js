a296410c2ccd041fc4613a8768a55b75722bb4e2
Givbygo <bogdanzaliskiy@gmail.com>
2020-05-26 21:45:42 +0300

    checking yarn material

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6fbfeea4534bb4e4364abaa8bbae6b61.js"
+  "/precache-manifest.05368ecdafa7fc1ca95a435eb5aaa42c.js"
 );
 
 self.addEventListener('message', (event) => {
